4fd6e10fc01e4fff98bf6385f83506ef45009907
RoKo1984 <kotlyarrs@gmail.com>
2022-02-04 20:36:00 +0200

    Домашнее задание №3 задачи 2 - 10. (нумерация соответствует файлу PDF)

--- HW_3_task_10.py
+++ HW_3_task_10.py
@@ -1,8 +1,10 @@
 
 dic1 = dict()
 for x in range(1, 16):
-    dic1[x] = x**2   # можно ли каким-то образом записать это выражение через **= ?
+    dic1[x] = x**2
 
 print(dic1)
 
-
+# Вариант записи №2
+dic1 = {a: a ** 2 for a in range(1, 16)}
+print(dic1)
