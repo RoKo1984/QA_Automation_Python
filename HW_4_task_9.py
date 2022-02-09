6c01b5b463396ee6ca3fc91bf3fada73c9011552
RoKo1984 <kotlyarrs@gmail.com>
2022-02-09 13:38:20 +0200

    Домашнее задание №4 задачи 1 - 9.

--- HW_4_task_9.py
+++ HW_4_task_9.py
@@ -1,15 +1,11 @@
 # Реализовать логику Difference для двух списков (list), проверить работу
 # алгоритма на set.difference
 
-l1 = [10, 20, 30, 40, 50]
-l3 = [50, 75, 30, 20, 40, 69]
-
 
 import random
 
 LIST_SIZE = 5
 RANDOM_UPPER_BOUND = 10
-
 
 a = []
 b = []
@@ -25,7 +21,7 @@ c = []
 for x in a:
     if x not in b:
         res.append(x)
-        for item in res:
+        for item in res:    # ? не придумал как правильно записать условие, чтобы из получаемого списка удалить дубликаты, если они есть без ввода нового списка и еще одного вложенного условия
             if item not in c:
                 c.append(item)
 print()
@@ -34,6 +30,7 @@ print()
 # print(f'Результат = {list(set(res))}') # вариант записи для того чтобы исключить повторяющиеся цифры в списке. Только нужно закоментировать строки 28 - 30
 print()
 
+# Проверка по set.difference
 a = set(a)
 b = set(b)
 z = a.difference(b)
