89715c484e5f5ae6432f642464d303d0af37bf53
RoKo1984 <kotlyarrs@gmail.com>
2022-02-09 16:41:40 +0200

    Домашнее задание №5

---
+++ HW_5_task_2.py
@@ -0,0 +1,25 @@
+# Напишите функцию Python для суммирования всех чисел в списке.
+
+import random
+
+LIST_SIZE = 5
+RANDOM_UPPER_BOUND = 10
+list = []
+
+for _ in range(0, LIST_SIZE):
+    list.append(random.randint(0, RANDOM_UPPER_BOUND))
+
+print(f'список:  {list}')
+
+def sum(list):
+
+
+    s = 0
+    for i in list:
+        s = s + i
+    return print(f'сумма чисел списка равна: sum = {s}')
+
+sum(list)
+
+
+
