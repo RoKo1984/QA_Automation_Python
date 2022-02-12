74445e83633fc8c60367507b200fceb64bbb4a38
RoKo1984 <kotlyarrs@gmail.com>
2022-02-12 23:24:52 +0200

    Домашнее задание №3 задачи 2 - 10.

--- HW_3_task_2.py
+++ HW_3_task_2.py
@@ -1,3 +1,9 @@
+# Напишите программу Python, которая принимает слово от
+# пользователя и переворачивает его
+# Пример:
+# input - Hello Worlds
+# output - sdlroW olleH
+
 word = input("Input a word to reverse: ")
 
 for indx in range( len(word) - 1, -1, -1):
--- HW_3_task_3.py
+++ HW_3_task_3.py
@@ -1,3 +1,7 @@
+# Напишите программу Python для построения следующего шаблона,
+# используя вложенный цикл for.
+
+
 Height =  int(input("Введите высоту треугольника: "))
 for line_number in range(1, Height + 1):
         print("*" * line_number)
