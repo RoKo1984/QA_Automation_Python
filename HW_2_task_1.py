585f49943672a5ebe712f6599ea25a17f83836be
RoKo1984 <kotlyarrs@gmail.com>
2022-02-12 23:23:28 +0200

    Домашнее задание №2 задачи 1 - 7

--- HW_2_task_1.py
+++ HW_2_task_1.py
@@ -1,3 +1,8 @@
+# За день машина проезжает n километров. Сколько дней нужно, чтобы
+# проехать маршрут длиной m километров? Программа получает на вход
+# числа n и m.
+
+
 n = float(input("Enter the distance that the car travels in one day in kilometers:  " ))
 m = float(input("Enter the total distance to be driven in kilometers:  " ))
 
